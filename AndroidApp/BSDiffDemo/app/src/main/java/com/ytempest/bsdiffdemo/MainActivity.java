16dd7f04a04cd45015f590677b54d5318bc0de10
ytempest <787491096@qq.com>
2018-02-11 22:15:28 +0800

    实现增量更新的功能

--- AndroidApp/BSDiffDemo/app/src/main/java/com/ytempest/bsdiffdemo/MainActivity.java
+++ AndroidApp/BSDiffDemo/app/src/main/java/com/ytempest/bsdiffdemo/MainActivity.java
@@ -1,13 +1,65 @@
 package com.ytempest.bsdiffdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.ytempest.bsdiffdemo.util.PatchUtils;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String mNewApkPath = Environment.getExternalStorageDirectory().getAbsolutePath()
+            + File.separator + "version_2.0.apk";
+
+    private String mPatchPath = Environment.getExternalStorageDirectory().getAbsolutePath()
+            + File.separator + "version_1.0_2.0.patch";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void update(View view) {
+        if (!new File(mPatchPath).exists()) {
+            Toast.makeText(this, "patch包不存在", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        // 1、获取本地的getPackageResourcePath()apk路径
+        String currentApkPath = getPackageResourcePath();
+
+        // 2、下载完差分包之后，调用我们的方法去合并生成新的apk
+        // 是一个耗时操作，怎么弄 开线程+，Handler, AsyncTask , RXJava
+
+
+        ///3、合并差分包生成新版本的APK，新版本的APK路径为 mNewApkPath
+        PatchUtils.combine(currentApkPath, mNewApkPath, mPatchPath);
+
+        ///4、检验签名，就是获取本地apk的签名，与我们新版本的apk作对比
+        try {
+            String currentApkSignature = PatchUtils.getSignature(getPackageResourcePath());
+            String newApkSignature = PatchUtils.getSignature(mNewApkPath);
+            if (currentApkSignature.equals(newApkSignature)) {
+                Toast.makeText(this, "检验签名成功", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "检验签名失败，请检查安装包", Toast.LENGTH_SHORT).show();
+                return;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        ///5、安装新版本的Apk
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setDataAndType(Uri.fromFile(new File(mNewApkPath)),
+                "application/vnd.android.package-archive");
+        startActivity(intent);
+        Toast.makeText(this, "更新完成", Toast.LENGTH_SHORT).show();
     }
 }
--- AndroidApp/BSDiffDemo/app/src/main/java/com/ytempest/bsdiffdemo/util/PatchUtils.java
+++ AndroidApp/BSDiffDemo/app/src/main/java/com/ytempest/bsdiffdemo/util/PatchUtils.java
@@ -1,19 +1,107 @@
 package com.ytempest.bsdiffdemo.util;
 
+import android.content.pm.Signature;
+import android.os.Build;
+import android.util.DisplayMetrics;
+
+import java.io.File;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 /**
- * Email 240336124@qq.com
- * Created by Darren on 2017/5/6.
- * Version 1.0
- * Description:
+ * @author ytempest
+ *         Description: 实现增量更新，合并差分包
  */
 public class PatchUtils {
-    static{
+    static {
         System.loadLibrary("bspatch");
     }
+
+
     /**
-     * @param oldApkPath  原来的apk  1.0 本地安装的apk
-     * @param newApkPath  合并后新的apk路径   需要生成的2.0
-     * @param patchPath  差分包路径， 从服务器上下载下来
+     * 获取该目录下的apk签名
+     *
+     * @param path 当前apk路径
      */
-    public static native void combine(String oldApkPath,String newApkPath,String patchPath);
+    public static String getSignature(String path) throws ReflectiveOperationException {
+        // 1.反射实例化PackageParser对象
+        Object packageParser = getPackageParser(path);
+
+        // 2.反射获取parsePackage方法
+        Object packageObject = getPackageInfo(path, packageParser);
+
+        // 3.调用collectCertificates方法
+        Method collectCertificatesMethod = packageParser.getClass()
+                .getDeclaredMethod("collectCertificates", packageObject.getClass(), int.class);
+
+        collectCertificatesMethod.invoke(packageParser, packageObject, 0);
+
+        // 4.获取mSignatures属性
+        Field signaturesField = packageObject.getClass().getDeclaredField("mSignatures");
+        signaturesField.setAccessible(true);
+        Signature[] mSignatures = (Signature[]) signaturesField.get(packageObject);
+        return mSignatures[0].toCharsString();
+    }
+
+
+    /**
+     * 创建PackageParser.Package类，兼容5.0
+     */
+    private static Object getPackageInfo(String path, Object packageParser) throws ReflectiveOperationException{
+        Class<?>[] paramClass;
+        Object[] paramObject;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            paramClass = new Class[2];
+            paramClass[0] = File.class;
+            paramClass[1] = int.class;
+
+            paramObject = new Object[2];
+            paramObject[0] = new File(path);
+            paramObject[1] = 0;
+        } else {
+            paramClass = new Class[4];
+            paramClass[0] = File.class;
+            paramClass[1] = String.class;
+            paramClass[2] = DisplayMetrics.class;
+            paramClass[3] = int.class;
+
+            paramObject = new Object[4];
+            paramObject[0] = new File(path);
+            paramObject[1] = path;
+            DisplayMetrics metrics = new DisplayMetrics();
+            metrics.setToDefaults();
+            paramObject[2] = metrics;
+            paramObject[3] = 0;
+        }
+
+        Method parsePackageMethod = packageParser.getClass().getDeclaredMethod("parsePackage", paramClass);
+        parsePackageMethod.setAccessible(true);
+        return parsePackageMethod.invoke(packageParser, paramObject);
+    }
+
+
+    /**
+     * 创建PackageParser类
+     */
+    private static Object getPackageParser(String path) throws ReflectiveOperationException {
+        Class<?> packageParserClazz = Class.forName("android.content.pm.PackageParser");
+        // 兼容5.0版本
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Constructor<?> packageParserConstructor = packageParserClazz.getDeclaredConstructor();
+            return packageParserConstructor.newInstance();
+        } else {
+            Constructor<?> packageParserConstructor = packageParserClazz.getDeclaredConstructor(String.class);
+            return packageParserConstructor.newInstance(path);
+        }
+    }
+
+
+    /**
+     * @param oldApkPath 原来的apk，本地安装的apk
+     * @param newApkPath 新的apk路径
+     * @param patchPath  差分包路径，从服务器上下载下来
+     */
+    public static native void combine(String oldApkPath, String newApkPath, String patchPath);
 }
