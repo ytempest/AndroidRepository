6f01b65aad99818d40a4b5649641f96a3eea6df6
ytempest <787491096@qq.com>
2018-03-01 15:36:41 +0800

    not thing

--- AndroidApp/SourceCode/app/src/main/java/com/ytempest/sourcecode/MainActivity.java
+++ AndroidApp/SourceCode/app/src/main/java/com/ytempest/sourcecode/MainActivity.java
@@ -2,6 +2,7 @@ package com.ytempest.sourcecode;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
